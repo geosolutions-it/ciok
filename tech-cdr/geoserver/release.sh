48e055dbe117607d2f7bcf18978fdb1b2413f89d
carlo.cancellieri@fao.org <carlo.cancellieri@fao.org@7ef53a88-c6ab-11e1-8b2a-f91badb9ac58>
2013-04-03 08:24:44 +0000

    include release script
    git-svn-id: https://svn.fao.org/projects/techcdr/faodata-core/geosolutions/trunk@75667 7ef53a88-c6ab-11e1-8b2a-f91badb9ac58

---
+++ tech-cdr/geoserver/release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TAG="tech-cdr-geoserver-2.2.5_RC14"
+
+#git pull origin master
+#./changeVersion -c . ${TAG}
+#nano pom.xml
+#git add -u
+#git commit -m "release ${TAG}"
+#git tag -a ${TAG} -m "release ${TAG}"
+#git svn dcommit
+#git svn tag ${TAG} -m "release ${TAG}"
+
+#check tag here https://svn.fao.org/projects/techcdr/faodata-core/geosolutions/tags/
+
+#./changeVersion -c . tech-cdr-geoserver-2.2-SNAPSHOT
+#nano pom.xml
+#git add -u
+#git commit -m "preparing for the next tech-cdr geoserver release iteration"
+#git pull origin master
+#git push origin master --tags
+#git svn dcommit
